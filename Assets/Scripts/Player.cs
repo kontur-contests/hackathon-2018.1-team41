0ec2a4c32f658a84cd661097a60e7d4bda7e2790
Andrey Kolkov <androndo@gmail.com>
2018-04-07 20:55:36 +0400

    fix health bar, add gameover

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
 	[SerializeField]
 	private float speed;
+
+	private float damage = 0;
+
+    private const float fireDamage = 30;
+    private const float criticalDamage = 300;
 
 	private Vector2 direction;
 	private Vector2 lastDirection;
@@ -19,6 +25,7 @@ public class Player : MonoBehaviour {
 	void Update () {
 		GetInput ();
 		Move ();
+	    updateDamage();
 	}
 
 	void Move() {
@@ -44,6 +51,27 @@ public class Player : MonoBehaviour {
 
 		lastDirection = direction;
 	}
+
+
+    private void updateDamage() {
+        var damageBar = GameObject.Find("healthFade");
+        damageBar.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, damage);
+        //damageBar.transform.position = new Vector3(390 - damage/2, damageBar.transform.position.y, damageBar.transform.position.z);
+       
+        
+    }
+
+    private void addDamage(float volume) {
+        damage += volume;
+        if (damage >= criticalDamage)
+        {
+            var gameOver = GameObject.Find("gameOver");
+            var image = gameOver.GetComponent<Image>();
+            var tempColor = image.color;
+            tempColor.a = 1f;
+            image.color = tempColor;
+        }
+    }
     
 	void OnCollisionEnter2D(Collision2D coll)
 	{
@@ -51,7 +79,9 @@ public class Player : MonoBehaviour {
 		if (coll.gameObject.tag == "Fire") {
 			Debug.Log("get damage on fire");
 
-			if (lastDirection == Vector2.up) {
+		    addDamage(fireDamage);
+
+            if (lastDirection == Vector2.up) {
 				direction = Vector2.down;
 			}
             else if (lastDirection == Vector2.down) {
